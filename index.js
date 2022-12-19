2eb23011f00d45a2c4d1ba71080099d5f22ad093
hienphan0111 <hienphan@digitalcad.edu.vn>
2022-12-19 21:38:54 +0700

    Add toggle menu

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const btn_close = document.querySelector('.btn-close');
-const menu = document.querySelector('.menu');
-let isClick = false;
+const btn_close = document.getElementById('btn-close');
+const menu = document.getElementById('menu');
+const hbgMenu = document.querySelector('.hamberger');
+const overlay = document.querySelector('.overlay');
+const itemsMenuToggle = document.querySelectorAll('.menu > li');
 
-btn_close.addEventListener('click', function(event){
-    if(isClick)
-    {
-        menu.classList.add('invisible');
-        isClick = !isClick;
-    }
-    else {
-        menu.classList.remove('invisible');
-        isClick = !isClick;
-    }
+const closeToggleMenu = () => {
+    overlay.classList.add('hidden');
+    menu.classList.remove('menu-toggle');
+    btn_close.classList.add('hidden');
+    hbgMenu.classList.remove('hidden');
+}
+
+btn_close.addEventListener('click', closeToggleMenu);
+
+
+hbgMenu.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    hbgMenu.classList.add('hidden');
+    btn_close.classList.remove('hidden');
+    menu.classList.add('menu-toggle');
+})
+
+itemsMenuToggle.forEach((item)=>{
+    item.addEventListener('click', closeToggleMenu);
 })
