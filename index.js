f8ce6c945f254fe643ab9d6ce2d6485dec600206
hienphan0111 <hienphan@digitalcad.edu.vn>
2022-12-19 21:52:30 +0700

    Fix linter err

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const btn_close = document.getElementById('btn-close');
+const btnClose = document.getElementById('btn-close');
 const menu = document.getElementById('menu');
 const hbgMenu = document.querySelector('.hamberger');
 const overlay = document.querySelector('.overlay');
 const itemsMenuToggle = document.querySelectorAll('.menu > li');
 
 const closeToggleMenu = () => {
-    overlay.classList.add('hidden');
-    menu.classList.remove('menu-toggle');
-    btn_close.classList.add('hidden');
-    hbgMenu.classList.remove('hidden');
-}
+  overlay.classList.add('hidden');
+  menu.classList.remove('menu-toggle');
+  btnClose.classList.add('hidden');
+  hbgMenu.classList.remove('hidden');
+};
 
-btn_close.addEventListener('click', closeToggleMenu);
-
+btnClose.addEventListener('click', closeToggleMenu);
 
 hbgMenu.addEventListener('click', () => {
-    overlay.classList.remove('hidden');
-    hbgMenu.classList.add('hidden');
-    btn_close.classList.remove('hidden');
-    menu.classList.add('menu-toggle');
-})
+  overlay.classList.remove('hidden');
+  hbgMenu.classList.add('hidden');
+  btnClose.classList.remove('hidden');
+  menu.classList.add('menu-toggle');
+});
 
-itemsMenuToggle.forEach((item)=>{
-    item.addEventListener('click', closeToggleMenu);
-})
+itemsMenuToggle.forEach((item) => {
+  item.addEventListener('click', closeToggleMenu);
+});
