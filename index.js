642ffeb45a587e99f095c40d6eeddacab5796ee2
Ferizad <dossanovafm@gmail.com>
2022-12-20 15:31:23 +0600

    Modified JS file

--- index.js
+++ index.js
@@ -23,3 +23,53 @@ hbgMenu.addEventListener('click', () => {
 itemsMenuToggle.forEach((item) => {
   item.addEventListener('click', closeToggleMenu);
 });
+
+
+const projects = [
+    {
+        name: 'Tonic',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        featureImage: "./image/pr1.png",
+        technologies: ['html', 'css', 'javascript'],
+        liveVersion: '',
+        resource: ''
+    },
+    {
+        name: 'Multi-Post Stories',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        featureImage: "./image/pr1.png",
+        technologies: ['html', 'css', 'javascript'],
+        liveVersion: '',
+        resource: ''
+    },
+    {
+        name: 'Facebook 360',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        featureImage: "./image/pr1.png",
+        technologies: ['html', 'css', 'javascript'],
+        liveVersion: '',
+        resource: ''
+    }
+]
+const _renderPopup = (event, pjIndex) => {
+    const { name,  description } = projects[pjIndex];
+    let html = `
+    <div class='model-container'>
+        <h1>${ name }</h1>
+        <p>${ description }</p>
+    </div> `;
+    popup.insertAdjacentHTML('afterend', html);
+}
+if(btn.length > 1){
+    btn.forEach((item,index)=>{
+        item.addEventListener('click', (event)=>{
+        const { name,  description } = projects[index];
+        let html = `
+            <div class='model-container'>
+            <h1>${ name }</h1>
+            <p>${ description }</p>
+            </div> `;
+            popup.innerHTML = html;
+        });
+    });
+}
